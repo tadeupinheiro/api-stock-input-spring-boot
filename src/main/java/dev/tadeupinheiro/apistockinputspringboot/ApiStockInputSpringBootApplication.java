c223378a02ad00a14aedad75bd06885791d3fce1
Tadeu Pinheiro <tadeujr2014@gmail.com>
2024-01-04 21:37:30 -0300

    Spring Initializr

---
+++ src/main/java/dev/tadeupinheiro/apistockinputspringboot/ApiStockInputSpringBootApplication.java
@@ -0,0 +1,13 @@
+package dev.tadeupinheiro.apistockinputspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiStockInputSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiStockInputSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/tadeupinheiro/apistockinputspringboot/ApiStockInputSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tadeupinheiro.apistockinputspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiStockInputSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
